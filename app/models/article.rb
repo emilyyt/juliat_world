98c6a266f19fc83c52ac19eb878cb28b26229850
Emily Tsai <emily.c.tsai@gmail.com>
2012-04-26 21:07:50 -0400

    added controller and views

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
 	belongs_to :category
 
 
-	validate_presence_of :title, :content
+	validates_presence_of :title, :content
 
 
 	scope :alphabetical, order('title')
